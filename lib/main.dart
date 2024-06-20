fd16e25cdc2232484ae2157157f884be3aaa6261
Youssab El Khanagry <subamichael28@gmail.com>
2024-06-20 19:11:53 +0300

    WeatherPage added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:weather/screen/weather_screen.dart';
 
 void main() {
   runApp(
@@ -13,6 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
+      home: WeatherPage(),
     );
   }
 }
