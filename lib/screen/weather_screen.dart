5b07c6ee87fa144dcffebc9c3531c38d2277ab98
Youssab El Khanagry <subamichael28@gmail.com>
2024-06-22 19:52:27 +0300

    modifications

--- lib/screen/weather_screen.dart
+++ lib/screen/weather_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:weather/models/weather_model.dart';
+import 'package:weather/service/weather_service.dart';
 
 class WeatherPage extends StatefulWidget {
   const WeatherPage({super.key});
@@ -8,8 +10,42 @@ class WeatherPage extends StatefulWidget {
 }
 
 class _WeatherPageState extends State<WeatherPage> {
+  // apiKey
+  final _weatherService =
+      WeatherService(apiKey: "a0e9c84bda6c46b4180a2e997727c40c");
+  Weather? _weather;
+
+  // Fetch weather
+  _fetchWeather() async {
+    String cityName = await _weatherService.getCurrentCity();
+    try {
+      final weather = await _weatherService.getWeather(cityName);
+      setState(() {
+        _weather = weather;
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchWeather();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(_weather?.cityName??"loading city..."),
+            Text("${_weather?.temperature.round()} °C"),
+          ],
+        ),
+      ),
+    );
   }
 }
